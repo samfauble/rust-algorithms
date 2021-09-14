a597e0734d7b6cce45c3e5a0a4db634d605c2783
Sam <sam@blackarcs.org>
2021-09-14 16:17:46 -0300

    initial

---
+++ target/rls/debug/deps/algorithms-35914a935bab9ca3.d
@@ -0,0 +1,5 @@
+c:\Users\samfa\Documents\rustPractice\algorithms\target\rls\debug\deps\algorithms-35914a935bab9ca3.rmeta: src\main.rs
+
+c:\Users\samfa\Documents\rustPractice\algorithms\target\rls\debug\deps\algorithms-35914a935bab9ca3.d: src\main.rs
+
+src\main.rs:
---
+++ target/rls/debug/deps/algorithms-f898a17251eb9d83.d
@@ -0,0 +1,5 @@
+c:\Users\samfa\Documents\rustPractice\algorithms\target\rls\debug\deps\algorithms-f898a17251eb9d83.rmeta: src\main.rs
+
+c:\Users\samfa\Documents\rustPractice\algorithms\target\rls\debug\deps\algorithms-f898a17251eb9d83.d: src\main.rs
+
+src\main.rs:
