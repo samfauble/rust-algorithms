41ed10d67c8019065a70cd189f87aeb2cc593f88
Sam <sam@blackarcs.org>
2021-09-19 14:14:50 -0300

    edit random and docs

--- src/randomized.rs
+++ src/randomized.rs
@@ -17,7 +17,7 @@ pub mod randomized_algos {
         if exp == 0.0 {return 1.0;}                 //base case
         let y = (exp / 2.0).floor();
         let z = mod_exponent(base, y, modulo);      //recurse     
-        if exp % 2.0 == 0.0 {                       //fi exponent is even
+        if exp % 2.0 == 0.0 {                       //if exponent is even
             z.powf(2.0) % modulo
         } else {
             (base * z).powf(2.0) % modulo
@@ -39,24 +39,39 @@ pub mod randomized_algos {
         }
     }
 
+    /**
+     * Euclid's extended algorithm (EEA) is used to calculate GCD and multiplicative inverses.
+     * If GCD(x, y) = 1 where (x % y), then EEA can give us the inverse (x^-1 % y).
+     * In the result, alpha % y = x^-1 % y. 
+     */
     pub fn euclid_gcd_ext(x: u128, y: u128) -> ExtEuclidRes{
         if y == 0 {
-            ExtEuclidRes{alpha: 1, beta: 0, gcd: x}
+            ExtEuclidRes{alpha: 1, beta: 0, gcd: x}                         //base case
         } else {
-            let res = euclid_gcd_ext(y, x % y);
-            let beta_prime = res.alpha - ((x/y) * res.beta);
-            ExtEuclidRes{alpha: res.alpha, beta: beta_prime, gcd: res.gcd}
+            let res = euclid_gcd_ext(y, x % y);                             //recurse
+            let beta_prime = res.alpha - ((x/y) * res.beta);                //calc beta'
+            ExtEuclidRes{alpha: res.alpha, beta: beta_prime, gcd: res.gcd}  //result
         }
     }
 
+    /**
+     * The simple primality algorithm is used to test for whether a value x is prime.
+     * x is prime if it is relatively prime with all numbers from 1 - (x-1),
+     * so this algorithm tests for relative primality of x with a random value r.
+     * 
+     * If the two values aren't relatively prime, then r acts as a Fermat witness to
+     * the fact that x is not prime. If they are relatively prime, x might be prime,
+     * but we can't be certain.
+     * 
+     * For this implementation, true is returned if x might be prime.
+     */
     pub fn simple_primality(x: u32, rounds: usize) -> bool{
         let mut res: bool = true;
         
         for _i in 0..rounds {
             let r = thread_rng().gen_range(1..x);
-            let z = x.pow(r);
-            let modulo = 1 % r;
-            if z != modulo as u32 {
+            let z = mod_exponent(x as f64, (r-1) as f64, r as f64);
+            if z as u64 != (1 % r) as u64 {
                 res = false;
                 break;
             }
@@ -65,5 +80,5 @@ pub mod randomized_algos {
         res
     }
 
-    pub fn carmichael_nums(){}
+    pub fn carmichael_nums() {}
 }
